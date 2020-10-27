3bf090d3a50c07a13055e37ffdb2187c7a7e10b4
tammy <tammy@sleet.eol.ucar.edu>
2020-10-27 17:45:49 -0600

    Adding script file

--- mysci.py
+++ mysci.py
@@ -1,9 +1,34 @@
-#Read the data file
-filename = "data/wxobs20170821.txt"
+print("Hello, world!")
 
+#Read the data file and look at first 4 lines
+#filename = "data/wxobs20170821.txt"
+#datafile = open(filename, 'r')
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+#datafile.close()
+
+#Read entire data file
+filename = "data/wxobs20170821.txt"
+datafile = open(filename, 'r')
+data = datafile.read()
+datafile.close()
+
+#DEBUG
+#print all the data
+print(data)
+#print the word data
+print('data')
+
+
+filename = "data/wxobs20170821.txt"
 with open(filename, 'r') as datafile:
     data = datafile.read()
 
+#DEBUG
+print(data)
+print(type(data))
 
 #DEBUG
 #print(type(data)); tells you data type
