10ff3e2074471044cc5e847d0234c57d19e44fed
tammy <tammy@sleet.eol.ucar.edu>
2020-10-28 13:47:35 -0600

    Converting tempout to floats

--- mysci.py
+++ mysci.py
@@ -16,5 +16,4 @@ with open(filename, 'r') as datafile:
        split_line = line.split()
        data['date'].append(split_line[0])
        data['time'].append(split_line[1])
-       data['tempout'].append(split_line[2])
-      
+       data['tempout'].append(int(float(split_line[2])))
